705572509b50f144bb17cf084b9da42c9b4b4031
agusdwimilniadi <agusdwimill@gmail.com>
2022-11-02 00:06:47 +0700

    push

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  var typed = new Typed(".typing-2", {
+    strings: ["Beauty", "Glorious", "Greats", "Magnificent", "Marvelous"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  const nav = document.querySelector("#navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) nav.style.background = "#232631";
+    else nav.style.background = "transparent";
+  });
+});
