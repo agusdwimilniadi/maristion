c12d3f66ef72dbab656c3b7d2846ad974bb4b9eb
agusdwimilniadi <agusdwimill@gmail.com>
2022-11-09 21:26:59 +0700

    fix bug

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,34 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
+  $(".flickity-index").flickity({
+    autoPlay: true,
+    prevNextButtons: false,
+    pageDots: false,
+  });
+});
+
+$(document).ready(function () {
+  $(".flicity-stories-1").flickity({
+    autoPlay: 1540,
+    prevNextButtons: false,
+    pageDots: false,
+  });
+});
+$(document).ready(function () {
+  $(".flicity-stories-2").flickity({
+    wrapAround: 200,
+    autoPlay: true,
+    prevNextButtons: false,
+    pageDots: false,
+  });
+});
+$(document).ready(function () {
+  $(".flicity-stories-3").flickity({
+    autoPlay: 1400,
+    prevNextButtons: false,
+    pageDots: false,
+  });
 });
 const nav = document.querySelector("#navbar");
 
