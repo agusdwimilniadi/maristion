6a895f23509b2c4eb6b6de9d2e2c9b4b32889749
agusdwimilniadi <agusdwimill@gmail.com>
2022-11-10 11:54:48 +0700

    selesai

--- assets/script.js
+++ assets/script.js
@@ -5,37 +5,9 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
-  $(".flickity-index").flickity({
-    autoPlay: true,
-    prevNextButtons: false,
-    pageDots: false,
-  });
 });
 
-$(document).ready(function () {
-  $(".flicity-stories-1").flickity({
-    autoPlay: 1540,
-    prevNextButtons: false,
-    pageDots: false,
-  });
-});
-$(document).ready(function () {
-  $(".flicity-stories-2").flickity({
-    wrapAround: 200,
-    autoPlay: true,
-    prevNextButtons: false,
-    pageDots: false,
-  });
-});
-$(document).ready(function () {
-  $(".flicity-stories-3").flickity({
-    autoPlay: 1400,
-    prevNextButtons: false,
-    pageDots: false,
-  });
-});
 const nav = document.querySelector("#navbar");
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) nav.style.background = "#232631";
   else nav.style.background = "transparent";
